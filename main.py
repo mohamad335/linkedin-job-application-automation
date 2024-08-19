9ff9ce7e1b5712797b8282b2d6a1b3ab75f99eaf
Mohammad <mhamdl2amen@gmail.com>
2024-08-19 14:18:46 +0300

    add try and except for all job apply

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from selenium.webdriver.common.by import By
 import time
 from dotenv import load_dotenv
 import os
+from selenium.common.exceptions import NoSuchElementException
 load_dotenv()
+
 email = os.getenv("MY_EMAIL")
 password = os.getenv("MY_PASSWORD")
 phone_number=os.getenv("PHONE_NUMBER")
@@ -14,6 +16,7 @@ driver.get( "https://www.linkedin.com/jobs/search/?f_LF=f_AL&geoId=102257491"
     "&keywords=python%20developer"
     "&location=London%2C%20England%2C%20United%20Kingdom"
     "&redirect=false&position=1&pageNum=0")
+
 time.sleep(2)
 driver.find_element(By.LINK_TEXT,"Sign in").click()
 time.sleep(5)
@@ -24,14 +27,18 @@ my_password.send_keys(password)
 submit=driver.find_element(By.CSS_SELECTOR, "#organic-div > form > div.login__form_action_container > button")
 submit.click()
 input("Press Enter when you have solved the Captcha")
+easy_pply= driver.find_elements(By.CLASS_NAME,"artdeco-button__text")
 time.sleep(5)
-company_name = driver.find_element(By.ID, "3960496109")
-company_name.click()
-time.sleep(5)
-easy_pply= driver.find_element(By.CLASS_NAME,"artdeco-button__text")
-easy_pply.click()
-time.sleep(5)
-phone=driver.find_element(By.CLASS_NAME,"fb-dash-form-element__error-field artdeco-text-input--input")
-phone.send_keys(phone_number)
-time.sleep(5)
-next=driver.find_element(By.CLASS_NAME,"artdeco-button__text")
+for i in easy_pply:
+    if i.text=="Easy Apply":
+        i.click()
+        time.sleep(5)
+    try:
+         phone=driver.find_element(By.CLASS_NAME,"fb-dash-form-element__error-field artdeco-text-input--input")
+         if phone.text=="":
+             phone.send_keys(phone_number)
+             time.sleep(5)
+         next=driver.find_element(By.CLASS_NAME,"artdeco-button__text")
+         next.click()
+    except NoSuchElementException:
+         print("No application, button,skipped")
